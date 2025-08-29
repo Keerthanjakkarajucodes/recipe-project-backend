833a282009b4ccaac52bba48c0c424c66cb38f98
Keerthan Jakkaraju <keerthan14.codes@gmail.com>
2025-08-29 18:09:38 +0530

    Added change password controller and route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerUser,loginUser,getProfile} from "../controllers/userController.js";
+import {registerUser,loginUser,getProfile,changePassword} from "../controllers/userController.js";
 import {protect} from "../middleware/authMiddleware.js"; 
 
 const router=express.Router();
